1f8e2eeabc74a87d1a329128da157f57b2447ee9
Tayná Martins Ramos <tayna.ramos@ifood.com.br>
2025-03-14 22:29:15 -0300

    remover prints

--- bin/introducao_dart.dart
+++ bin/introducao_dart.dart
@@ -9,19 +9,13 @@ void main(List<String> arguments) async {
   await findAll(client);
 
   await create(client, CreatePostModel(id: 1, body: "teste", title: "uau"));
-  print('------');
   await create(client, CreatePostModel(id: 2, body: "teste2", title: "uau2"));
-  print('------');
   await create(client, CreatePostModel(id: 3, body: "teste3", title: "uau3"));
-  print('------');
   await findAll(client);
-  print('------');
   await deleteOne(client, 2);
-  print('------');
   await updateOne(
     client,
     CreatePostModel(id: 1, body: "body atualizado", title: "novo title"),
   );
-  print('------');
   await findAll(client);
 }
