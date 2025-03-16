80c28c97f8295b1426111114fcdc83c4ae50d8b4
Tayná Martins Ramos <tayna.ramos@ifood.com.br>
2025-03-16 17:05:30 -0300

    Testando widgets e estilização

--- packages/my_first_app_web/lib/main.dart
+++ packages/my_first_app_web/lib/main.dart
@@ -4,6 +4,10 @@ void main() {
   runApp(const MyApp());
 }
 
+void onPressed() {
+  print('onPressed');
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,7 +17,40 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: Text("Meu primero app flutter")),
         body: Center(
-          child: Text("Olá flutter", style: TextStyle(fontSize: 26, fontWeight: FontWeight.w700)),
+          child: Column(
+            children: [
+              Text("column1"),
+              Text("column2"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text("row1"),
+                  Text("row2"),
+                  Container(
+                    color: Colors.red,
+                    child: Text("row3", style: TextStyle(color: Colors.white)),
+                  ),
+                ],
+              ),
+              Container(color: Colors.amber, child: Text("container1")),
+              Container(
+                color: Colors.purple,
+                width: 48.0,
+                height: 48.0,
+                margin: const EdgeInsets.all(10.0),
+              ),
+              ElevatedButton(
+                onPressed: onPressed,
+                child: Icon(
+                  Icons.favorite,
+                  color: Colors.pink,
+                  size: 24.0,
+                  semanticLabel: 'Text to announce in accessibility modes',
+                ),
+              ),
+              ListTile(leading: Icon(Icons.map), title: Text("Mapa", ),)
+            ],
+          ),
         ),
       ),
     );
